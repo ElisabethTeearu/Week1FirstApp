6255d09efb487875202073df18b634786e14b02e
Elisabeth Teearu <elisabeth282000@gmail.com>
2024-10-06 19:25:42 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// My first app.
+
+string myName = "Elisabeth";
+string myLastName = "Teearu";
+
+//Console.WriteLine("Hello" + " " +, myName);
+
+Console.WriteLine($"Hello, {myName} {myLastName}!");
